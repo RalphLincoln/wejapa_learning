689e0240dcc263f19ab8b3762284f7e0f2a8bf80
Alaneme Ikenna Raphael <ialaneme@yahoo.com>
2020-08-23 04:39:59 +0100

    add a preview page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
 import { Switch, Route } from 'react-router-dom';
 
 
 
-const HatsPage = () => (<div>Hats Page</div>);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
 
+import SHOP_DATA from "./shop.data";
+
+import CollectionPreview from "../../components/preview-collection/collection-preview.component";
+
 export default class ShopPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
   render() {
-    return <div></div>;
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
   }
 }
